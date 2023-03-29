2dffa7bda8a2c5c4a112fd1e4d82bdcc7614f05a
tschelovek <az18rg@gmail.com>
2023-03-29 13:21:18 +0300

    layout 80%

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// import Swiper, {Pagination} from 'swiper';
+
+const swiper = new Swiper('#slider_hero', {
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  speed: 1000,
+  effect: 'coverflow'
+})
