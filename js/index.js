39258f73b5e5cf533632a1802ab33c0fd28953ee
tschelovek <az18rg@gmail.com>
2023-03-31 21:11:49 +0300

    ready

--- js/index.js
+++ js/index.js
@@ -1,13 +1,42 @@
-// import Swiper, {Pagination} from 'swiper';
+document.addEventListener('DOMContentLoaded', () => {
+  /**
+   *
+   * @type {Swiper} Инициализация слайдера
+   */
+  const swiper = new Swiper('#slider_hero', {
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+    speed: 1000,
+    // effect: 'coverflow'
+    // autoHeight: true
+  })
 
-const swiper = new Swiper('#slider_hero', {
-  pagination: {
-    el: '.swiper-pagination',
-  },
-  loop: true,
-  autoplay: {
-    delay: 3000,
-  },
-  speed: 1000,
-  // effect: 'coverflow'
+  /**
+   *
+   * Бургер меню
+   */
+  document.getElementById('burgerButton')?.addEventListener('click', (e) => {
+    e.preventDefault();
+    e.currentTarget.classList.toggle('active');
+    document.querySelector('.header__navigation__list')?.classList.toggle('active');
+  })
+
+  /**
+   * Скукоживатель шапки при прокрутке
+   */
+  window.addEventListener('scroll', (e) => {
+    const header = document.body.querySelector('header');
+    if(window.scrollY > 0) {
+      !header.classList.contains('shrink') ? header.classList.add('shrink') : null
+    } else {
+      header.classList.remove('shrink')
+    }
+  })
+  //*На случай перезагрузки страницы проверяем положение и если надо вешаем скукоживатель на шапку
+  if (window.scrollY > 0) document.body.querySelector('header')?.classList.add('shrink')
 })
